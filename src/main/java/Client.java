019f056f33486a7f9cb0fead2ded865a9531a86b
teodorro <edem.teo@gmail.com>
2020-08-16 01:41:45 +0300

    .

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -14,8 +14,7 @@ public class Client {
         int port = 8645;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader( new
-                     InputStreamReader(clientSocket.getInputStream()))) {
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             out.println("vasya");
             String resp = in.readLine();
             System.out.println(resp);
